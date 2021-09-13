1d69bda954175448d2ca9aed2d18a9f8cdd96d33
Raston Gilbert <rastong@gmail.com>
2021-09-12 20:53:14 -0400

    initial commit

---
+++ Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Fidget = 3.5;
+            double Memes = 5;
+            bool choice = false;
+            do
+            {
+                Console.Write("Please enter amount.");
+                double amount = int.Parse(Console.ReadLine());
+
+                Console.Write("Please enter measurement type: fidget spinners, memes, inches, or foot.");
+                string measure = (Console.ReadLine());
+
+                choice = false;
+
+                if (measure == "fidget spinners")
+                {
+                    amount /= Fidget;
+                    Console.WriteLine(amount + " Inches");
+
+                    Console.WriteLine("Would you like another conversion: y or n?");
+                    if (Console.ReadLine() == "y")
+                    {
+                        choice = true;
+                    }
+                }
+                else if (measure == "memes")
+                {
+                    amount /= Memes;
+                    Console.WriteLine(amount + " Feet");
+
+                    Console.WriteLine("Would you like another conversion: y or n?");
+                    if (Console.ReadLine() == "y")
+                    {
+                        choice = true;
+                    }
+                }
+                else if (measure == "foot")
+                {
+                    amount *= Memes;
+                    Console.WriteLine(amount + " Memes");
+
+                    Console.WriteLine("Would you like another conversion: y or n?");
+                    if (Console.ReadLine() == "y")
+                    {
+                        choice = true;
+                    }
+                }
+                else if (measure == "inches")
+                {
+                    amount *= Fidget;
+                    Console.WriteLine(amount + " fidget spinners");
+
+                    Console.WriteLine("Would you like another conversion: y or n?");
+                    if (Console.ReadLine() == "y")
+                    {
+                        choice = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter enter fidget spinner, memes, inch, or foot for the measurement types");
+
+                    Console.WriteLine("Would you like another conversion: y or n?");
+                    if (Console.ReadLine() == "y")
+                    {
+                        choice = true;
+                    }
+                }
+            } while (choice);
+
+            Console.WriteLine("Have a nice day.");
+        }
+    }
+}
